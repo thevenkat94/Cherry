43097f8c9822b4349c8a806332d10d43dad596ef
Venkat <prabhuvenkat46@gmail.com>
2024-11-30 13:31:05 +0530

    print("test merge to beta 2")

--- Pick/Pick/ViewController.swift
+++ Pick/Pick/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("test merge to beta")
+        print("test merge to beta 2")
     }
 
 
